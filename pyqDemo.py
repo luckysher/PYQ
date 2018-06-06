8e73d15ab275bca6ba8db368ce24ad9d4159919f
luckysher <luckyshera95@gmail.com>
2018-06-06 22:14:37 +0530

    added method for adding label in the GUI

--- pyqDemo.py
+++ pyqDemo.py
@@ -7,8 +7,11 @@ class PYQDemo:
     def __init__(self):
         self.app = QtGui.QApplication(sys.argv)
 
-    def startGUI(self):
+    def addLabel(self, lableStr, pox, poy):
+        b = QtGui.QLabel()
+        b.setText("test label")
 
+    def startGUI(self):
         w.show()
         sys.exit(self.app.exec_())
 
